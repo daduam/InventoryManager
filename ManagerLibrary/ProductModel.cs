00ea71e6b54c7a0bd7fe7a329be8c329e638aad5
Joseph Ampadu <jkampadu002@st.ug.edu.gh>
2022-08-27 00:39:39 +0100

    Add models

---
+++ ManagerLibrary/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerLibrary
+{
+    public enum ProductStatus { Active, Draft }
+
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public ProductCategoryModel Category { get; set; }
+        public ProductStatus Status { get; set; } = ProductStatus.Draft;
+        public string Description { get; set; }
+        public int ReorderLevel { get; set; }
+        public string SKU { get; set; }
+        public string Barcode { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Media { get; set; } = new List<string>();
+    }
+}
---
+++ ManagerLibrary/TillModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerLibrary
+{
+    public enum TillStatusType { Pending, Active, Closed, Verified }
+    public class TillModel
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public decimal OpeningBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
+        public UserModel Attendant { get; set; }
+        public TillStatusType Status { get; set; }
+    }
+}
